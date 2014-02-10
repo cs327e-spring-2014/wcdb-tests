c4609a22783989fd09cba17299c1444ea974c694
jihyunjayna.yoo@utexas.edu <jaynaj37@cs.utexas.edu>
2014-02-10 17:48:12 -0600

    another commit

--- TestXML.py
+++ TestXML.py
@@ -10,6 +10,8 @@ print()
 =======
 >>>>>>> 4a3c794bc81c8bc230a4a46524ce9a0eb5c8a3c9
 
+import io
+import unittest
 
 
 from xml.etree.ElementTree import Element, fromstring, tostring
@@ -22,7 +24,9 @@ from xml.etree.ElementTree import Element, fromstring, tostring
 print("TestXML.py")
 print()
 
+
 def traverse (a, d = "") :
+    assert (len.(a) != 0)
     print(d + a.tag)
     for v in a :
         traverse(v, d + "\t")
@@ -30,6 +34,7 @@ def traverse (a, d = "") :
 
 s = "<xml>" + "".join(open("Input.xml")) + "</xml>"
 assert(type(s) is str)
+assert(len.type(s) != 0)
 
 a = fromstring(s)
 assert(type(a) is Element)
@@ -38,3 +43,6 @@ traverse(a)
 print()
 
 print("Done.")
+
+
+
--- XML.py
+++ XML.py
@@ -1,3 +1,28 @@
+
+
 #--------------
 # XML.py
 #--------------
+
+from xml.etree.ElementTree import Element, fromstring, tostring
+
+print("XML.py")
+print()
+
+def traverse (a, d = "") :
+    print(d + a.tag)
+    for v in a :
+        traverse(v, d + "\t")
+    print(d + "/" + a.tag)
+
+s = "<xml>" + "".join(open("Input.xml")) + "</xml>"
+assert(type(s) is str)
+
+a = fromstring(s)
+assert(type(a) is Element)
+
+traverse(a)
+print()
+
+print("Done.")
+
