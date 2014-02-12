e55eb10709c1084e5fb26e287ae05e03aa9af1ae
jaynaj37 <jihyunjayna.yoo@utexas.edu>
2014-02-12 12:12:08 -0600

    another commit

--- XML.py
+++ XML.py
@@ -1,9 +1,8 @@
-
 #--------------
 # XML.py
 #--------------
 
-from xml.etree.ElementTree import Element, fromstring, tostring
+from xml.etree.ElementTree import Element, fromstring, tostring, tostringlist
 
 print("XML.py")
 print("")
@@ -15,7 +14,7 @@ def traverse (a, d = "") :
         traverse(v, d + "\t")
     print(d + "/" + a.tag)
 
-        print(len(a))
+        
     
 s = "<xml>" + "".join(open("Input.xml")) + "</xml>"
 assert(type(s) is str)
@@ -26,6 +25,9 @@ assert(type(a) is Element)
 traverse(a)
 
 
+for n in a.findall("./"):
+    m = tostringlist(n[0])
+    print(m)
 
 print("")
 
