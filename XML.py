08d979632c6060b204695d1d5d7d85cf73925346
jaynaj37 <jihyunjayna.yoo@utexas.edu>
2014-02-13 01:12:25 -0600

    another commit

--- XML.py
+++ XML.py
@@ -2,20 +2,44 @@
 # XML.py
 #--------------
 
-from xml.etree.ElementTree import Element, fromstring, tostring, tostringlist
+
+from xml.etree.ElementTree import Element, fromstring, tostring, SubElement
 
 print("XML.py")
 print("")
 x = 1
+tot = 0
 
-def traverse (a, d = "") :
+def numchild (a) :
+    n = 0
+    for v in a :
+        n += 1
+    return n    
+
+def match (a, b) :
+    if a.tag != b.tag :
+        return "false"
+    if numchild (b) == 0 :
+        return "true"
+    for v in a :
+        if numchild (b) > 0 :
+            if match (v, b[0]) == "true" :
+                return "true"
+    return "false"
+
+def traverse (a, opt) :
     global x
-    #print(d + a.tag + str(x))
+    global tot
+    
+    if match (a, q) == "true" :
+        if opt == 0 :
+            tot += 1
+        else :
+            print(str(x))
+
     for v in a :
         x += 1
-        traverse(v, d + "\t")
-    #print(d + "/" + a.tag)
-
+        traverse(v, opt)
     
 s = "<xml>" + "".join(open("Input.xml")) + "</xml>"
 assert(type(s) is str)
@@ -23,26 +47,16 @@ assert(type(s) is str)
 a = fromstring(s)
 assert(type(a) is Element)
 
-
+q = a[1]
 #for n in a.findall("."):
- #   traverse(n[1])
-
-traverse(a, "")
-
+    
+traverse(a[0], 0)
+print(tot)
 x = 1
-
-print ("Done.")
+traverse(a[0], 1)
 
 
 
 #def compare() :
  #   while (traverse(a[0], "")
-           
-
-           
-
-
-
-
-    
-
+ 
