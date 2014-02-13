eb87ddda8715c78a636e6fcdbb64d38eae138fb5
jihyunjayna.yoo@utexas.edu <jaynaj37@cs.utexas.edu>
2014-02-12 20:54:33 -0600

    another commit

--- XML.py
+++ XML.py
@@ -2,18 +2,20 @@
 # XML.py
 #--------------
 
-from xml.etree.ElementTree import Element, fromstring, tostring
+from xml.etree.ElementTree import Element, fromstring, tostring, tostringlist
 
 print("XML.py")
 print("")
-
+x = 1
 
 def traverse (a, d = "") :
-    print(d + a.tag)
+    global x
+    #print(d + a.tag + str(x))
     for v in a :
+        x += 1
         traverse(v, d + "\t")
-    print(d + "/" + a.tag)
-
+    #print(d + "/" + a.tag)
+    
     
 s = "<xml>" + "".join(open("Input.xml")) + "</xml>"
 assert(type(s) is str)
@@ -22,10 +24,24 @@ a = fromstring(s)
 assert(type(a) is Element)
 
 
+#for n in a.findall("."):
+ #   traverse(n[1])
 
-for n in a.findall("."):
-    traverse(n[1])
+traverse(a[0], "")
+    
+x = 1
+
+print ("Done.")
 
 
-print("Done.")
+#def compare() :
+ #   while (traverse(a[0], "")
+           
+
+           
+
+
+
+
+    
 
