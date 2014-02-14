fe4927521482c9af05e230d1ca8d32c32fa292ad
jaynaj37 <jihyunjayna.yoo@utexas.edu>
2014-02-13 22:39:06 -0600

    fixed the merging conflict

--- XML.py
+++ XML.py
@@ -2,30 +2,12 @@
 # XML.py
 #--------------
 
-<<<<<<< HEAD
-from xml.etree.ElementTree import Element, fromstring, tostring, tostringlist
-=======
 
 from xml.etree.ElementTree import Element, fromstring, tostring
 import sys
->>>>>>> b07f09d2af8d6add3d3cdf9e98d428c6d6367848
 
 print("")
 x = 1
-<<<<<<< HEAD
-
-def traverse (a, d = "") :
-    global x
-    #print(d + a.tag + str(x))
-    for v in a :
-        x += 1
-        traverse(v, d + "\t")
-    #print(d + "/" + a.tag)
-
-        p = iter(v)
-        e = next (p)
-        print(e.tag)
-=======
 tot = 0
 
 def numchild (a) :
@@ -58,45 +40,18 @@ def traverse (a, opt) :
     for v in a :
         x += 1
         traverse(v, opt)
->>>>>>> b07f09d2af8d6add3d3cdf9e98d428c6d6367848
 
-    
-s = "<xml>" + "".join() + "</xml>"
+
+s = "<xml>" + "".join(sys.stdin) + "</xml>"
 assert(type(s) is str)
 
 a = fromstring(s)
 assert(type(a) is Element)
 
-<<<<<<< HEAD
-
-#for n in a.findall("."):
- #   traverse(n[1])
-
-traverse(a[0], "")
-
-
-x = 1
-
-print ("Done.")
-
-
-
-#def compare() :
- #   while (traverse(a[0], "")
-           
-
-           
-
-
-
-
-    
-
-=======
 q = a[1]
     
 traverse(a[0], 0)
 print(tot)
 x = 1
 traverse(a[0], 1)
->>>>>>> b07f09d2af8d6add3d3cdf9e98d428c6d6367848
+
