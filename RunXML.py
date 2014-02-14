9d1170e2fcf534670a7c5d3fd6eaee03071afa05
jihyunjayna.yoo@utexas.edu <jaynaj37@cs.utexas.edu>
2014-02-14 14:03:30 -0600

    code trial after advice

--- RunXML.py
+++ RunXML.py
@@ -9,24 +9,24 @@
 
 
 import sys
-from xml.etree.ElementTree import Element, fromstring, tostring
 from XML import numchild, match, traverse
 
 #-------
 # main
 #-------
 
-s = "<xml>" + "".join(sys.stdin) + "</xml>"
+string = sys.stdin.readlines()
+s = "<xml>" + "".join(string) + "</xml>"
 assert(type(s) is str)
 
 a = fromstring(s)
 assert(type(a) is Element)
 
-q = a[1]
+q = a[len(a)]
     
 traverse(a[0], 0)
 print(tot)
 x = 1
 traverse(a[0], 1)
 
-print(sys.stdout)
+
