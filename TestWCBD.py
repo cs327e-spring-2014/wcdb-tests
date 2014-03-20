6603377b35c7f6766c1b28052dd6e16a1b4e5efc
nbmdss <nawel.boumerdassi@utexas.edu>
2014-03-19 23:38:17 -0500

    Added more tests

--- TestWCBD.py
+++ TestWCBD.py
@@ -12,18 +12,38 @@ class TestXML (unittest.TestCase) :
     def test_toElementTree_3 (self):
 
     def test_toString_1 (self):
+        s = "<parent><child></child></parent>"
+        x = fromstring(s)
+        l = toString(x)
+        self.assertTrue(l == "<parent><child></child></parent>")
+
+    def test_toString_2 (self):
+        s = "<parent></parent>"
+        x = fromstring(s)
+        l = toString(x)
+        self.assertTrue(l != "")
+        self.assertTrue(l == "<parent></parent>")
+
+    def test_toString_3 (self):
+        s = "<parent><child><grandchild></grandchild></child></parent>"
+        x = fromstring(s)
+        l = toString(x)
+        self.assertTrue(l != "")
+        self.assertTrue(l == "<parent><child><grandchild></grandchild></child></parent>")
+        
+    def test_WCBD_solve_1 (self):
         r = StringIO.StringIO("<parent><child></child></parent>")
         w = StringIO.StringIO()
         WCBD_solve(r, w)
         self.assertTrue(w.getValue() == "<parent><child></child></parent>")
 
-    def test_toString_2 (self):
+    def test_WCBD_solve_2 (self):
         r = StringIO.StringIO("<parent></parent>")
         w = StringIO.StringIO()
         WCBD_solve(r, w)
         self.assertTrue(w.getValue() == "<parent></parent>")
 
-    def test_toString_3 (self):
+    def test_WCBD_solve_3 (self):
         r = StringIO.StringIO("<parent><child><grandchild></grandchild></child></parent>")
         w = StringIO.StringIO()
         WCBD_solve(r, w)
