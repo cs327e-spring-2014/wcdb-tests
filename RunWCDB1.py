76c0d5ec81dce012bafaddfb63fa3b86a3dc00f7
jihyunjayna.yoo@utexas.edu <jaynaj37@cs.utexas.edu>
2014-03-27 13:36:48 -0500

    simplified code

---
+++ RunWCDB1.py
@@ -0,0 +1,34 @@
+
+from xml.etree.ElementTree import Element, fromstring, tostring
+
+def WCDB_run(r):
+    """
+transforms a string into an ElementTree
+s is a string
+a is an element
+return an Element
+"""
+    s = r.readline()
+    assert(type(s) is str)
+    a = fromstring(s)
+    assert(type(a) is Element)
+    return a
+
+    l = tostring(a)
+    assert(type(l) is str)
+    return l
+
+    if l in ["", "\n", None]:
+        return l
+        break
+
+def WCDB_solve(r,w):
+    """
+reads an input file per block of Element tree until the end of the file
+return string
+"""
+    while True:
+        output = WCDB_run(r)
+        if output in ["", "\n", None]:
+            break
+        w.write(output + "\n")
