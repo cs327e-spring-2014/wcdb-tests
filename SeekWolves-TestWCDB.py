1f8df915868b48951056a0157536de6ff0a34e1d
Havilland Voss <hgvoss@att.net>
2014-04-17 16:55:08 -0500

    Update SeekWolves-TestWCDB.py

--- SeekWolves-TestWCDB.py
+++ SeekWolves-TestWCDB.py
@@ -3,12 +3,12 @@
 # -------------------------------
 # projects/WCDB2/TestWCDB2.py
 # Copyright (C) 2014
-# Andrea Curo
+# Andrea Curo, Nathanael Bonney
 # -------
 """
 To test the program:
-    % python TestWCDB2.py > TestWCDB2.out
-    % chmod ugo+x TestWCDB2.py
+% python TestWCDB2.py > TestWCDB2.out
+% chmod ugo+x TestWCDB2.py
 """
 
 # -------
@@ -19,7 +19,8 @@ import StringIO
 import unittest
 import _mysql
 from xml.etree.ElementTree import Element, fromstring, tostring
-from WCDB import createTables, login, query, dropAll, xmlRead, xmlElementTree, iterTree, xmlInsert, xmlExport
+from WCDB import createTables, login, query, dropAll, xmlRead, xmlElementTree, iterTree, xmlInsert, xmlExport, dataExist, clean, normTableTracker, removeDuplicates, subInsert 
+import WCDB
 
 # -------
 # globals
@@ -31,7 +32,15 @@ c = _mysql.connect(host = "z", user = "python1", passwd = "VEXYOWWk7X", db = "cs
 # classes
 # -------
 
-class TestWCDB2(unittest.TestCase):
+class TestWCDB3(unittest.TestCase):
+
+    # --------
+    # setUp - Runs before every test to ensure the database is consistent
+    # --------
+
+    def setUp(self):
+        w = StringIO.StringIO()
+        xmlRead(open("RunWCDB.in.xml" , "r"), c, w)
 
     # ------------
     # createTables
@@ -42,12 +51,30 @@ class TestWCDB2(unittest.TestCase):
 
     def test_createTables_2(self):
         t = query(c, "Show Tables")
-        #print (t)
-        st = (('citations',), ('contactInfos',), ('crises',), ('crisisCitations',), ('crisisOrgs',), ('crisisPeople',), ('crisisResources',), ('crisisUrls',), ('crisisWaysToHelp',), ('orgCitations',), ('orgContactInfos',), ('orgPeople',), ('orgUrls',), ('orgs',), ('people',), ('personCitation',), ('personCitations',), ('personUrls',), ('resources',), ('urls',), ('waysToHelp',))
+        st = (('citations',), ('contactInfos',), ('crises',), ('crisisCitations',), ('crisisOrgs',), ('crisisPeople',), ('crisisResources',), ('crisisUrls',), ('crisisWaysToHelp',), ('orgCitations',), ('orgContactInfos',), ('orgPeople',), ('orgUrls',), ('orgs',), ('people',), ('personCitations',), ('personUrls',), ('resources',), ('urls',), ('waysToHelp',))
         self.assertTrue(sorted(t) == sorted(st))
 
     def test_createTables3(self):
         self.assertTrue(not createTables(1))
+
+    # --------
+    # dropAll
+    # --------
+
+    def test_dropAll_1(self):
+        self.assertTrue(dropAll(c))
+
+    def test_dropAll_2(self):
+        dropAll(c)
+        self.assertTrue(WCDB.activeTables == [])
+
+    def test_dropAll_3(self):
+        dropAll(c)
+        self.assertTrue(WCDB.syntax == {})
+
+    def test_dropAll_4(self):
+        dropAll(c)
+        self.assertTrue(WCDB.values == {})
 
 
     # --------
@@ -69,7 +96,7 @@ class TestWCDB2(unittest.TestCase):
     def test_query_2(self):
         s = "Show Tables"
         t = query(c, s)
-        self.assertTrue(t == (('citations',), ('contactInfos',), ('crises',), ('crisisCitations',), ('crisisOrgs',), ('crisisPeople',), ('crisisResources',), ('crisisUrls',), ('crisisWaysToHelp',), ('orgCitations',), ('orgContactInfos',), ('orgPeople',), ('orgUrls',), ('orgs',), ('people',), ('personCitation',), ('personCitations',), ('personUrls',), ('resources',), ('urls',), ('waysToHelp',)))
+        self.assertTrue(t == (('citations',), ('contactInfos',), ('crises',), ('crisisCitations',), ('crisisOrgs',), ('crisisPeople',), ('crisisResources',), ('crisisUrls',), ('crisisWaysToHelp',), ('orgCitations',), ('orgContactInfos',), ('orgPeople',), ('orgUrls',), ('orgs',), ('people',), ('personCitations',), ('personUrls',), ('resources',), ('urls',), ('waysToHelp',)))
 
     def test_query_3(self):
         s = "Show Databases"
@@ -77,7 +104,7 @@ class TestWCDB2(unittest.TestCase):
         self.assertTrue(t == (('information_schema',), ('cs327e_python1',)))
 
     # --------
-    # xmlRead
+    # xmlRead 
     # --------
     def test_read1(self):
         t = "<A><B></B></A>"
@@ -197,12 +224,6 @@ class TestWCDB2(unittest.TestCase):
         result = xmlRead(t, c, w)
         st = str(result)
         self.assertTrue(st == "<A><B></B><C></C><D></D><E></E><F></F><G></G><H></H><I></I><J></J><K></K><L></L><M></M><N></N></A>")
-    # --------
-    # dropAll
-    # --------
-
-    def test_dropAll_1(self):
-        self.assertTrue(dropAll(c))
 
     # --------
     # xmlElementTree
@@ -230,11 +251,99 @@ class TestWCDB2(unittest.TestCase):
         self.assertTrue(xmlInsert(c))
 
     # ---------
+    # xmlsubInsert
+    # ---------
+
+    def test_subInsert_1(self):
+        self.assertTrue(subInsert(0, "", c, "'PER_002', 'CIT_010',,", "personCitations") == 1)
+
+    def test_subInsert_2(self):
+        self.assertTrue(subInsert(12, "", c, "'PER_004', 'CIT_050',,", "personCitations") == 13)
+
+    def test_subInsert_3(self):
+        self.assertFalse(subInsert(0, 0, 0, 0, 0))
+
+    # ---------
     # xmlImport
     # ---------
     def test_xmlExport_1(self):
         w = StringIO.StringIO()
         self.assertTrue(xmlExport(c,w))
+
+    # ---------
+    # removeDuplicates
+    # ---------
+    def test_removeDuplicates_1(self):
+        self.assertTrue(removeDuplicates(c))
+
+    def test_removeDuplicates_2(self):
+        removeDuplicates(c)
+        s = "select * from crises;"
+        t = query(c, s)
+        viewedRows = []
+        for row in t:
+            self.assertNotIn(row, viewedRows)
+            viewedRows.append(row)
+        self.assertTrue(True)
+
+    def test_removeDuplicates_3(self):
+        removeDuplicates(c)
+        s = "select * from people;"
+        t = query(c, s)
+        viewedRows = []
+        for row in t:
+            self.assertNotIn(row, viewedRows)
+            viewedRows.append(row)
+        self.assertTrue(True)
+        
+    def test_removeDuplicates_4(self):
+        removeDuplicates(c)
+        s = "select * from orgs;"
+        t = query(c, s)
+        viewedRows = []
+        for row in t:
+            self.assertNotIn(row, viewedRows)
+            viewedRows.append(row)
+        self.assertTrue(True)
+
+    # ---------
+    # clean (normalize)
+    # ---------
+    def test_clean_1(self):
+        self.assertTrue(clean(c))
+
+    # ---------
+    # dataExist
+    # ---------
+    def test_dataExists_1(self):
+        name = "UT Tower Shooting"
+        table = "crises"
+        column = "name"
+        self.assertTrue(dataExist(name, table, c, column))
+
+    def test_dataExists_2(self):
+        name = "Lehman Brothers"
+        table = "orgs"
+        column = "name"
+        self.assertTrue(dataExist(name, table, c, column))
+        
+    def test_dataExists_3(self):
+        name = "Barack Obama"
+        table = "people"
+        column = "name"
+        self.assertTrue(dataExist(name, table, c, column))
+
+    def test_dataExists_4(self):
+        name = "Financial Crisis of 2038"
+        table = "crises"
+        column = "name"
+        self.assertFalse(dataExist(name, table, c, column))
+
+    # ---------
+    # normTableTracker
+    # ---------
+    def test_normTableTracker_1(self):
+        self.assertTrue(normTableTracker("orgContactInfos", 0, "URL", "ORG_002", c, 0))
 
 
 print ("TestWCDB.py")
